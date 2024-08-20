a4034c4a14e6d5b8e015e55779841bbe1d7a5401
Noah Cohn <noahc42@gmail.com>
2024-08-20 15:26:01 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from 'react'
+import { useState, useRef, useEffect, useCallback,useLayoutEffect } from 'react'
 
 function App() {
 useLayoutEffect(() => {
