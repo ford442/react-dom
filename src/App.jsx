86d9aa13866c1ba9acac647323ae192728469ed5
Noah Cohn <noahc42@gmail.com>
2025-02-13 04:54:44 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -69,6 +69,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
             const fullAudioBlob = new Blob(audioChunksRef.current, { type: 'audio/wav' });
             audioChunksRef.current = []; // Clear for next recording
             try {
+              /*
                 const app = await Client("ford442/facebook-fastspeech2-en-ljspeech", { hf_token: "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" });
                const result = await app.predict("/predict", [fullAudioBlob]); // Corrected call
               console.log(result)
@@ -82,6 +83,7 @@ navigator.mediaDevices.getUserMedia({ audio: true })
                 downloadLink.download = 'synthesized_audio.wav';
                 downloadLink.textContent = 'Download Audio';
                 document.body.appendChild(downloadLink);
+                */
             } else {
               console.error("No audio data received from the server.");
             }
