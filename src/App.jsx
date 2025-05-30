8c480d4ef50b0f9a2e683211f2e03af12a28a3ee
Noah Cohn <noahc42@gmail.com>
2025-05-29 20:08:44 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ useLayoutEffect(() => {
 const appConfig = {
   model_list: [
     {
-      model: "wasm-TinyLlama-1.1B-Chat-q4f342_1",
-      model_id: "cfahlgren1/wasm-TinyLlama-1.1B-Chat-q4f342_1",
-      model_lib_url: "https://huggingface.co/TinyLlama-1.1B-Chat-v1.0-q4f32_1-webgpu.wasm",
+      model: "https://huggingface.co/cfahlgren1/wasm-TinyLlama-1.1B-Chat-q4f342_1",
+      model_id: "wasm-TinyLlama-1.1B-Chat-q4f342_1",
+      model_lib_url: "https://huggingface.co/cfahlgren1/TinyLlama-1.1B-Chat-v1.0-q4f32_1-webgpu.wasm",
     },
   ],
 };
