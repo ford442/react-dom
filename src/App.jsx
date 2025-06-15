f394e89c4c1ba8004bfdcaee8fa9068a0db0c2ad
Noah Cohn <noahc42@gmail.com>
2025-06-15 02:41:12 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -591,7 +591,7 @@ useLayoutEffect(() => {
     console.log('Forcing remote settings and disabling cache for loading.');
     env.localFilesOnly = false;
     env.allowLocalModels = false;
-    env.useBrowserCache = false; 
+    env.useBrowserCache = true; 
     env.remoteHost = 'https://huggingface.co';
     env.remotePathTemplate = '{model}/resolve/main/';
     // env.wasm.numThreads = 16;
