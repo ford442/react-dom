c338aa59de2d417a745b598c58d68618a40c79e2
Noah Cohn <noahc42@gmail.com>
2025-06-15 19:30:10 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -299,6 +299,8 @@ max={2.0}
 <iframe src={'./bezz.1ink'} id={'circle'} title='Circular mask'></iframe>
 <input type={'button'} id={'generateButton'} style={{backgroundColor:'gold',position:'absolute',display:'block',left:'3%',top:'3%',zIndex:3200,border:'4px solid #e7e7e7',borderRadius:'27%'}}></input>
 <input type={'button'} id={'startBtn'} style={{backgroundColor:'gold',position:'absolute',display:'block',left:'6%',top:'9%',zIndex:3200,border:'4px solid #e7e7e7',borderRadius:'17%'}}></input>
+<input type={'button'} id={'startBtnB'} style={{backgroundColor:'gold',position:'absolute',display:'block',left:'6%',top:'19%',zIndex:3200,border:'4px solid #e7e7e7',borderRadius:'17%'}}></input>
+<input type={'button'} id={'startBtnPM'} style={{backgroundColor:'gold',position:'absolute',display:'block',left:'16%',top:'9%',zIndex:3200,border:'4px solid #e7e7e7',borderRadius:'17%'}}></input>
 <input type={'button'} id={'menuBtn'} style={{backgroundColor:'black',position:'absolute',display:'block',left:'3%',top:'5%',zIndex:3200,border:'6px solid #e7e7e7',borderRadius:'20%'}}></input>
 <input type={'button'} id={'musicBtn'} style={{backgroundColor:'cyan',position:'absolute',display:'block',left:'3%',bottom:'5%',zIndex:3200,border:'6px solid green',borderRadius:'20%'}}></input>
 <input type={'button'} id={'startBtn5'} style={{backgroundColor:'yellow',position:'absolute',display:'block',left:'2%',top:'9%',zIndex:3200,border:'4px solid #e7e7e7',borderRadius:'17%'}}></input>
