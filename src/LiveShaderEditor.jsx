5a57680aabae49ad5834d4fcf20a2fb1dd3ee667
Noah Cohn <noahc42@gmail.com>
2025-06-16 07:27:15 -0700

    Update LiveShaderEditor.jsx

--- src/LiveShaderEditor.jsx
+++ src/LiveShaderEditor.jsx
@@ -6,13 +6,8 @@ import { buildShaderFromSnippets } from './shaderBuilder';
 import { useTexture } from './useTexture';
 const vertexShader = `
   #version 300 es
-
-  // `in` is the new keyword for attributes
   in vec2 a_position;
-
-  // `out` is the new keyword for varyings
   out vec2 v_texCoord;
-
   void main() {
     gl_Position = vec4((a_position * 2.0 - 1.0) * vec2(1, -1), 0.0, 1.0);
     v_texCoord = a_position;
