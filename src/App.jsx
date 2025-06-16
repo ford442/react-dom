aa061dd64abaacb3f4e89b7b9033061672b8f6a0
Noah Cohn <noahc42@gmail.com>
2025-06-15 20:36:51 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -680,7 +680,7 @@ async function loadModel() {
         setStatusMessage(prev => `${prev} Loading TTS model (Kokoro)...`);
         
         // This single line downloads and initializes the Kokoro TTS model.
-        const kokoroInstance = await KokoroTTS.from_pretrained('onnx-community/Kokoro-82M-v1.0-ONNX', {dtype: "q4f16", device: "wasm", });
+        const kokoroInstance = await KokoroTTS.from_pretrained('onnx-community/Kokoro-82M-v1.0-ONNX', {dtype: "fp32", device: "webgpu", });
         
         setKokoroTtsInstance(() => kokoroInstance);
         console.log("Kokoro TTS pipeline loaded successfully.");
