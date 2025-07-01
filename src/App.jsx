1ded0804355fb32f8883c95c2feb791de8f80584
Noah Cohn <noahc42@gmail.com>
2025-07-01 15:42:52 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useRef, useEffect, useCallback, useLayoutEffect } from 'react';
-import { pipeline, env, Tensor } from '@huggingface/transformers';
+// import { pipeline, env, Tensor } from '@huggingface/transformers';
+import { pipeline, env, Tensor } from "https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.6.0";
+
+
 import Box from '@mui/material/Box'; // Assuming you still use these
 import Slider from '@mui/material/Slider'; // Assuming you still use these
 import './App.css';
