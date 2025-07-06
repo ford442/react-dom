437b15f2c3eeeb031c15fb24c2b24ca0046e7135
Noah Cohn <noahc42@gmail.com>
2025-07-06 03:07:51 -0700

    Update analysis.js

--- src/services/analysis.js
+++ src/services/analysis.js
@@ -1,4 +1,4 @@
-import { pipeline } from '@xenova/transformers';
+import { pipeline } from '@huggingface/transformers';
 
 /**
  * Predicts future cryptocurrency prices using a pre-trained time-series model.
