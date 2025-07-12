d6bfd30cd645b640f908f02f2fbfec2a05a6beae
Noah Cohn <noahc42@gmail.com>
2025-07-12 14:18:41 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path' // 👈 Make sure to import 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      'ossos': path.resolve(__dirname, './node_modules/ossos/src/ossos.ts')
+    }
+  }
+})
