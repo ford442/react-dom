e2cc0f4687f845505c80f0f49092aef4b826deff
Noah Cohn <noahc42@gmail.com>
2025-07-12 17:17:53 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1035,8 +1035,15 @@ max={2.0}
 <div id={'contain1'}>
 <canvas className='emscripten' id={'scanvas'} style={{pointerEvents:'auto',display:'block',position:'absolute',zIndex:3000,backgroundColor:'rgba(233,233,233,1.0)',top:'0',height:'100vh',width:'100vh',imageRendering:'auto',transform:'scaleY(1.0)'}}></canvas>
 
+<div className="canvas-container">
+      <div 
+        ref={mountRef} 
+        className="centered-square" 
+        style={{ width: '100vh', height: '100vh' }} // Example fixed size for your square
+      />
 <div ref={mountRef} className="three-container" style={{ position: 'absolute',display:'block', top: '0', width: '100vh', height: '100vh', zIndex: 3301, pointerEvents: 'auto' }} />
 </div>
+
 
 {/* =================================================================== */}
 {/* NEW, CLEANED-UP UI PANEL - REPLACES ALL THE OVERLAPPING DIVS */}
