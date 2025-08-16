6ecd1f44895abe2695591ac783621bfdf936e2eb
Noah Cohn <noahc42@gmail.com>
2025-08-15 23:43:32 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const example = document.getElementById('example');
 status.textContent = 'Loading model...';
 
 // const depth_estimator = await pipeline('depth-estimation', 'onnx-community/depth-anything-v2-large', { dtype: 'fp32', device: 'webgpu' });
-const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-large-hf', { dtype: 'fp16', device: 'webgpu',executionProviders: ['webgpu'] });
+const depth_estimator = pipeline('depth-estimation', 'Xenova/depth-anything-large-hf', { dtype: 'fp16', device: 'webgpu',executionProviders: ['webgpu'] });
 // const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-base-hf', { dtype: 'fp16', device: 'webgpu' });
 // const depth_estimator = await pipeline('depth-estimation', 'Xenova/depth-anything-small-hf',{dtype:'fp32',device:'webgpu',executionProviders: ['webgpu']});
 
