a24a484292342f1ce2cd073b45bd5284999e3ac7
Noah Cohn <noahc42@gmail.com>
2025-08-17 02:57:41 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
   
   return (
     <>
-      <link charset={"utf-8"} crossorigin rel='stylesheet' href='https://css.1ink.us/birdsong.1iss'/>
+      <link charset={"utf-8"} crossorigin rel='stylesheet' href='https://css.1ink.us/mamejs.1iss'/>
       <img id={'splash1'} src={'./image/shroud.jpg'} style={{backgroundColor:'rgba(233,233,233,0.0)',display:'block',position:'absolute',height:'100vh',width:'100vw',zIndex:3590}}></img>
       <img id={'splash2'} src={'./image/spinner.gif'} style={{backgroundColor:'rgba(47,47,47,1.0)',display:'block',top:'50%',left:'50%',transform:'translate(-50%,-50%)',position:'absolute',height:'20vh',width:'20vh',zIndex:3591}}></img>
       <main id={'panel'}>
