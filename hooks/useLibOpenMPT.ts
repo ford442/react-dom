354f20ca9c016cd5262c12b0744725e96d908c25
Noah Cohn <noahc42@gmail.com>
2025-11-11 15:24:03 -0800

    Update useLibOpenMPT.ts

--- hooks/useLibOpenMPT.ts
+++ hooks/useLibOpenMPT.ts
@@ -57,6 +57,7 @@ export function useLibOpenMPT() {
     setPatternData(ended ? '... Song Ended ...' : '... Stopped ...');
     if (ended) {
       var temp=patternData;
+      tempt=0;
         setStatus(`Finished playing "${moduleInfoRef.current.title}".`);
     }
   }, []);
